cebb8f8cff8c8bdb4552b9b4b581c9f51417d001
Conor-Gilbert <69896256+Conor-Gilbert@users.noreply.github.com>
2020-09-09 09:50:36 +0100

    before server

---
+++ source/grid.js
@@ -0,0 +1,253 @@
+const numRows = 6;
+const numColumns = 7;
+let isBlue = true;
+let squareColor = 'blue';
+let currentRow = '';
+let currentColumn = '';
+let col0Row = 5;
+let col1Row = 5;
+let col2Row = 5;
+let col3Row = 5;
+let col4Row = 5;
+let col5Row = 5;
+let col6Row = 5;
+let color = '';
+let win = false;
+const boardArray = [
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null],
+  [null, null, null, null, null, null]];
+
+function changeColor() {
+  if (currentRow >= 0) {
+    if (color === 'red') {
+      color = 'yellow';
+    } else {
+      color = 'red';
+    }
+  }
+  return color;
+}
+function createRow() {
+  const row = document.createElement('tr');
+  return row;
+}
+
+function createCell() {
+  const cell = document.createElement('td');
+
+  cell.style.height = '100px';
+  cell.style.width = '100px';
+  cell.style.backgroundColor = squareColor;
+
+  createCircle();
+
+  return cell;
+}
+
+function createCircle(circleid) {
+  const circle = document.createElement('td');
+  circle.style.display = 'block';
+  circle.style.margin = '0 auto';
+  circle.style.height = '50px';
+  circle.style.width = '50px';
+  circle.style.borderRadius = '50%';
+  circle.style.backgroundColor = 'white';
+
+  circle.id = circleid;
+
+  return circle;
+}
+
+const board = document.getElementById('board');
+
+$('#button0').click(() => {
+  console.log('Hello 0');
+  currentColumn = 0;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button1').click(() => {
+  console.log('Hello 1');
+  currentColumn = 1;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button2').click(() => {
+  console.log('Hello 2');
+  currentColumn = 2;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button3').click(() => {
+  console.log('Hello 3');
+  currentColumn = 3;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button4').click(() => {
+  console.log('Hello 4');
+  currentColumn = 4;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button5').click(() => {
+  console.log('Hello 5');
+  currentColumn = 5;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+$('#button6').click(() => {
+  console.log('Hello 6');
+  currentColumn = 6;
+  buttonPressedLog();
+
+  return currentColumn;
+});
+
+function buttonPressedLog() {
+  const cell = createCell(squareColor);
+  console.log('buttonPressed');
+
+  if (currentColumn === 0) {
+    currentRow = col0Row;
+    col0Row--;
+  }
+  if (currentColumn === 1) {
+    currentRow = col1Row;
+    col1Row--;
+  }
+  if (currentColumn === 2) {
+    currentRow = col2Row;
+    col2Row--;
+  }
+  if (currentColumn === 3) {
+    currentRow = col3Row;
+    col3Row--;
+  }
+  if (currentColumn === 4) {
+    currentRow = col4Row;
+    col4Row--;
+  }
+  if (currentColumn === 5) {
+    currentRow = col5Row;
+    col5Row--;
+  }
+  if (currentColumn === 6) {
+    currentRow = col6Row;
+    col6Row--;
+  }
+  const circleid = `circle-${currentRow}-${currentColumn}`;
+  console.log(circleid);
+  color = changeColor(color);
+  const circle = createCircle(circleid);
+  $(`#${circleid}`).css('background-color', color);
+  cell.appendChild(circle);
+  if (currentRow >= 0) {
+    boardArray[currentRow][currentColumn] = color;
+    console.log(boardArray[currentRow][currentColumn]);
+    checkWin();
+  }
+  console.log(color);
+}
+
+function verticalWin() {
+  if ((boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 2][currentColumn]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 3][currentColumn])) {
+    win = true;
+    console.log(win, 'vertical');
+  }
+  return win;
+}
+function horizontalWin() {
+  if ((boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 1])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow][currentColumn - 1])) {
+    win = true;
+    console.log(win, 'horizontal');
+  }
+  return win;
+}
+function diagonalWin() {
+  if ((boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 2][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 3][currentColumn + 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 2][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 3][currentColumn - 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 2][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn + 1])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 2][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn - 1])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 2][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 3][currentColumn + 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 2][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 3][currentColumn - 3])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow + 1][currentColumn - 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow + 2][currentColumn - 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn + 1])
+        || (boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn + 1]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 2][currentColumn + 2]
+        && boardArray[currentRow][currentColumn] === boardArray[currentRow - 1][currentColumn - 1])) {
+    win = true;
+    console.log(win, 'diagonal');
+  }
+  return win;
+}
+
+function checkWin() {
+  verticalWin();
+  horizontalWin();
+  diagonalWin();
+  if (win) {
+    winnerWinner();
+  }
+}
+
+function winnerWinner() {
+  console.log(color, ' is win');
+}
+for (let i = 0; i < numRows; i++) {
+  const row = createRow();
+  isBlue = i % 2 === 0;
+  for (let j = 0; j < numColumns; j++) {
+    if (isBlue) {
+      squareColor = 'blue';
+    } else {
+      squareColor = 'darkblue';
+    }
+    const cell = createCell(squareColor);
+    const circleid = `circle-${i}-${j}`;
+    const circle = createCircle(circleid);
+
+    row.appendChild(cell);
+    cell.appendChild(circle);
+
+    isBlue = !isBlue;
+  }
+  board.appendChild(row);
+}
